6e9efd64eb79570f6e2f82dcc065bc4bc329978c
zaveriparth <parth.zaveri@nordicglobal.com>
2024-06-20 18:36:02 -0400

    Update process.py

--- process.py
+++ process.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import os
 
 def process_csv(file_path):
     df = pd.read_csv(file_path)
@@ -7,5 +6,5 @@ def process_csv(file_path):
     df.to_csv(file_path, index=False)
 
 if __name__ == "__main__":
-    file_path = os.getenv('CSV_FILE_PATH', 'tempfile.csv')
+    file_path = 'input.csv'
     process_csv(file_path)
